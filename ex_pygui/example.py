cadf3f3005952d8b40553ea2f90537313c7e7fd0
chenlue <1833907216@qq.com>
2020-07-20 21:22:39 +0800

    add new example

--- ex_pygui/example.py
+++ ex_pygui/example.py
@@ -1,25 +1,19 @@
-#!usr/bin/python3
-#-*- coding: utf-8 -*-
-
-from PyQt5.QtCore import QUrl, QRect
+from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QButtonGroup, QFrame, QToolButton, QStackedLayout, \
-    QWidget, QStatusBar, QBoxLayout, QLabel, QDesktopWidget, QMessageBox, QMenu, QAction, QFileDialog, QLineEdit, \
-    QGridLayout, QComboBox
+    QWidget, QStatusBar, QBoxLayout, QLabel, QDesktopWidget, QMessageBox, QMenu, QAction
 from PyQt5.QtGui import QDesktopServices, QFont, QIcon
 from PyQt5.QtCore import Qt
 
 import sys
 
-combox_dict = {'combo1':1, 'combo1':2, 'combo3':3,
-               'combo4':4, 'combo5':5, 'combo6':6 }
 
-class Little_tool(QWidget):
-    def __init__(self, n = 1):
+class Demo(QWidget):
+    def __init__(self):
         super().__init__()
 
         self.setWindowTitle("测试")
         # 窗口大小
-        self.resize(600, 800)
+        self.resize(1400, 800)
         # self.setFixedSize(1500, 600)  # 设置窗口为固定尺寸， 此时窗口不可调整大小
         # self.setMinimumSize(1800， 1000)  # 设置窗口最大尺寸
         # self.setMaximumSize(900， 300)  # 设置窗口最小尺寸
@@ -27,10 +21,6 @@ class Little_tool(QWidget):
         # self.setWindowFlags(Qt.FramelessWindowHint)  # 设置无边框窗口样式，不显示最上面的标题栏
 
         self.content_font = QFont("微软雅黑", 12, QFont.Medium)  # 定义字体样式
-
-        self.language_dict = {}
-        self.layout_count = n
-        self.layout_list = []
 
         self.center()
 
@@ -124,56 +114,18 @@ class Little_tool(QWidget):
         # 创建堆叠布局
         self.stacked_layout = QStackedLayout(self.main_frame)
 
-        main_frame1 = self.window1_UI(self.main_frame)
-        main_frame2 = self.window2_UI(self.main_frame)
-
-        # 把两个布局放进去
-        self.stacked_layout.addWidget(main_frame1)
-        self.stacked_layout.addWidget(main_frame2)
-
-    def window1_UI(self, frame):
         # 第一个布局
-        main_frame1 = QMainWindow()
-        frame1_bar = QStatusBar()
-        frame1_bar.setObjectName("frame1_bar")
-        main_frame1.setStatusBar(frame1_bar)
-        frame1_bar.showMessage("欢迎进入frame1")
+        self.main_frame1 = QMainWindow()
+        self.frame1_bar = QStatusBar()
+        self.frame1_bar.setObjectName("frame1_bar")
+        self.main_frame1.setStatusBar(self.frame1_bar)
+        self.frame1_bar.showMessage("欢迎进入frame1")
 
-        self.rom_frame = QFrame(main_frame1)
-        self.rom_frame.setGeometry(0, 0, self.width(), frame.height() - 25)
-        self.rom_frame.setFrameShape(QFrame.Panel)
-        self.rom_frame.setFrameShadow(QFrame.Raised)
+        rom_frame = QFrame(self.main_frame1)
+        rom_frame.setGeometry(0, 0, self.width(), self.main_frame.height() - 25)
+        rom_frame.setFrameShape(QFrame.Panel)
+        rom_frame.setFrameShadow(QFrame.Raised)
 
-        # 创建布局管理器
-        self.layout1 = QBoxLayout(QBoxLayout.TopToBottom)
-        tab_lay = QBoxLayout(QBoxLayout.LeftToRight)
-        tab_lay1 = QBoxLayout(QBoxLayout.LeftToRight)
-
-        output_title = QLabel('dest', self.rom_frame)
-        output_title.setGeometry(self.width() * 0.1, self.height() * 0.1, 10, 4)
-        self.output_file = QLineEdit(self.rom_frame)
-        DirButton = QPushButton(self.rom_frame)
-        DirButton.setObjectName('query Button')
-        DirButton.setText('...')
-        DirButton.setStatusTip('Click to select file')
-        DirButton.clicked.connect(self.file_select)
-        addButton = QPushButton(self.rom_frame)
-        addButton.resize(5, 2)
-        addButton.setObjectName('add Button')
-        addButton.setText('+')
-        addButton.setStatusTip('Click to add source select')
-        addButton.clicked.connect(self.add_file_select)
-
-        output_title1 = QLabel('src1', self.rom_frame)
-        self.output_file1 = QLineEdit(self.rom_frame)
-        DirButton1 = QPushButton(self.rom_frame)
-        DirButton1.setObjectName('query Button')
-        DirButton1.setText('...')
-        DirButton1.setStatusTip('Click to select source file')
-        DirButton1.clicked.connect(self.file_select)
-        combo1 = QComboBox(self.rom_frame)
-
-        '''
         # 超链接
         self.super_link = QLabel(rom_frame)
         self.super_link.setText("""
@@ -183,61 +135,50 @@ class Little_tool(QWidget):
         self.super_link.setFont(self.content_font)  # 使用字体样式
         self.super_link.setOpenExternalLinks(True)  # 使其成为超链接
         self.super_link.setTextInteractionFlags(Qt.TextBrowserInteraction)  # 双击可以复制文本
-        '''
-        start_btn = QPushButton("开 始", self.rom_frame)
-        start_btn.setGeometry(self.width() * 0.3, self.height() * 0.85, 100, 40)
+
+        self.start_btn = QPushButton("开 始", rom_frame)
+        self.start_btn.setGeometry(self.width() * 0.7, self.height() * 0.8, 100, 40)
         # self.start_btn.clicked.connect(self.start_btn_click)
-        quit_btn = QPushButton("退 出", self.rom_frame)
-        quit_btn.setGeometry(self.width() * 0.6, self.height() * 0.85, 100, 40)
-        quit_btn.setStatusTip("点击关闭程序")
+        self.quit_btn = QPushButton("退 出", rom_frame)
+        self.quit_btn.setGeometry(self.width() * 0.85, self.height() * 0.8, 100, 40)
+        self.quit_btn.setStatusTip("点击关闭程序")
         # self.quit_btn.clicked.connect(QCoreApplication.instance().quit)  # 点击退出可以直接退出
-        quit_btn.clicked.connect(self.close)  # 点击退出按钮的退出槽函数
+        self.quit_btn.clicked.connect(self.close)  # 点击退出按钮的退出槽函数
 
-        tab_lay.addWidget(output_title, 1)
-        tab_lay.addWidget(self.output_file, 20)
-        tab_lay.addWidget(DirButton, 1)
-        tab_lay.addWidget(addButton, 1)
+        #rom_frame1 = QFrame()
+        #rom_frame1.setFrameShape(QFrame.Panel)
+        #rom_frame1.setFrameShadow(QFrame.Raised)
 
-        tab_lay1.addWidget(output_title1, 1)
-        tab_lay1.addWidget(self.output_file1, 20)
-        tab_lay1.addWidget(DirButton1, 1)
-        tab_lay1.addWidget(combo1, 1)
+        #rom_frame2 = QFrame()
+        #rom_frame2.setFrameShape(QFrame.Panel)
+        #rom_frame2.setFrameShadow(QFrame.Raised)
+
+        # 创建布局管理器
+        self.layout1 = QBoxLayout(QBoxLayout.TopToBottom)
 
         # 给管理器对象设置父控件
-        self.rom_frame.setLayout(self.layout1)
-        main_frame1.setCentralWidget(self.rom_frame)
+        rom_frame.setLayout(self.layout1)
+        self.main_frame1.setCentralWidget(rom_frame)
+
+        # 把子控件添加到布局管理器中
+        #self.layout1.addWidget(rom_frame1, 1)
+        #self.layout1.addWidget(rom_frame2, 1)
 
         self.layout1.setContentsMargins(0, 0, 0, 0)  # 设置布局的左上右下外边距
         self.layout1.setSpacing(0)  # 设置子控件的内边距
 
-        self.layout_list.append(tab_lay)
-        self.layout_list.append(tab_lay1)
+        frame1_bar_frame = QFrame(self.main_frame1)
+        frame1_bar_frame.setGeometry(0, self.main_frame.height(), self.width(), 25)
 
-        #self.layout1.addLayout(tab_lay)
-        #self.layout1.addStretch(1)  # 设置空间之间的上下间距
-        #self.layout1.addWidget(QLabel('please select the source file'))
-        #self.layout1.addLayout(tab_lay1)
-        #self.layout1.addStretch(1)  # 设置空间之间的上下间距
-
-        for i in range(len(self.layout_list)):
-            self.layout1.addLayout(self.layout_list[i])
-        self.layout1.addStretch(1)  # 设置空间之间的上下间距
-
-        frame1_bar_frame = QFrame(main_frame1)
-        frame1_bar_frame.setGeometry(0, frame.height(), self.width(), 25)
-
-        return main_frame1
-
-    def window2_UI(self, frame):
         # 第二个布局
-        main_frame2 = QMainWindow()
-        frame2_bar = QStatusBar()
-        frame2_bar.setObjectName("frame2_bar")
-        main_frame2.setStatusBar(frame2_bar)
-        frame2_bar.showMessage("欢迎进入frame2")
+        self.main_frame2 = QMainWindow()
+        self.frame2_bar = QStatusBar()
+        self.frame2_bar.setObjectName("frame2_bar")
+        self.main_frame2.setStatusBar(self.frame2_bar)
+        self.frame2_bar.showMessage("欢迎进入frame2")
 
-        custom_frame = QFrame(main_frame2)
-        custom_frame.setGeometry(0, 0, self.width(), frame.height() - 25)
+        custom_frame = QFrame(self.main_frame2)
+        custom_frame.setGeometry(0, 0, self.width(), self.main_frame.height() - 25)
         custom_frame.setFrameShape(QFrame.Panel)
         custom_frame.setFrameShadow(QFrame.Raised)
 
@@ -254,28 +195,31 @@ class Little_tool(QWidget):
         custom_frame3.setFrameShadow(QFrame.Raised)
 
         # 创建布局管理器
-        layout2 = QBoxLayout(QBoxLayout.TopToBottom)
+        self.layout2 = QBoxLayout(QBoxLayout.TopToBottom)
 
         # 给管理器对象设置父控件
-        custom_frame.setLayout(layout2)
+        custom_frame.setLayout(self.layout2)
         """
         使用了父类为QMainWindow的话，在里面使用布局类，QGridLayout， QHBoxLayout ，QVBoxLayout 等等时，发现不好用，
         加上下面这句代码就可以了，QMainWindow对象.setCentralWidget(这里填布局管理器的父控件对象)
         """
-        main_frame2.setCentralWidget(custom_frame)
+        self.main_frame2.setCentralWidget(custom_frame)
 
         # 把子控件添加到布局管理器中
-        layout2.addWidget(custom_frame1, 1)
-        layout2.addWidget(custom_frame2, 1)
-        layout2.addWidget(custom_frame3, 1)
+        self.layout2.addWidget(custom_frame1, 1)
+        self.layout2.addWidget(custom_frame2, 1)
+        self.layout2.addWidget(custom_frame3, 1)
 
-        layout2.setContentsMargins(0, 0, 0, 0)  # 设置布局的左上右下外边距
-        layout2.setSpacing(0)  # 设置子控件的内边距
+        self.layout2.setContentsMargins(0, 0, 0, 0)  # 设置布局的左上右下外边距
+        self.layout2.setSpacing(0)  # 设置子控件的内边距
 
-        frame2_bar_frame = QFrame(main_frame2)
-        frame2_bar_frame.setGeometry(0, frame.height(), self.width(), 25)
+        frame2_bar_frame = QFrame(self.main_frame2)
+        frame2_bar_frame.setGeometry(0, self.main_frame.height(), self.width(), 25)
 
-        return main_frame2
+        # 把两个布局放进去
+        self.stacked_layout.addWidget(self.main_frame1)
+        self.stacked_layout.addWidget(self.main_frame2)
+
 
     def click_window1(self):
         if self.stacked_layout.currentIndex() != 0:
@@ -296,58 +240,9 @@ class Little_tool(QWidget):
     def click_about(self, event):
         QMessageBox.about(self, "关于", "使用文档，请参考：xxxxxx")
 
-    def add_file_select(self):
-        self.layout_count += 1
-        if(self.layout_count > 8):
-            return
-        tab_lay_add = QBoxLayout(QBoxLayout.LeftToRight)
-
-        text = 'src%d' %(self.layout_count)
-        output_title_add = QLabel(text, self.rom_frame)
-        output_file_add = QLineEdit(self.rom_frame)
-        DirButton_add = QPushButton(self.rom_frame)
-        DirButton_add.setObjectName('query Button')
-        DirButton_add.setText('...')
-        DirButton_add.setStatusTip('Click to select source file')
-        DirButton_add.clicked.connect(self.file_select)
-        combo_add = QComboBox(self.rom_frame)
-
-        tab_lay_add.addWidget(output_title_add, 1)
-        tab_lay_add.addWidget(output_file_add, 20)
-        tab_lay_add.addWidget(DirButton_add, 1)
-        tab_lay_add.addWidget(combo_add, 1)
-
-        tab_lay_add.widgetEvent()
-
-        #self.layout1.addWidget(QLabel('please select the source file'))
-        #self.layout1.addLayout(tab_lay_add)
-        #self.layout1.addStretch(1)#设置空间之间的上下间距
-
-        self.layout_list.append(tab_lay_add)
-
-        for i in range(len(self.layout_list)):
-            self.layout1.addLayout(self.layout_list[i])
-            self.layout1.setStretch(2*i, 0)
-        self.layout1.addStretch(1)  # 设置空间之间的上下间距
-
-        self.rom_frame.update()
-
-    def file_select(self):
-        file, ok1 = QFileDialog.getOpenFileName(self,
-                                                  "多文件选择",
-                                                  "./",
-                                                  "All Files (*);;Text Files (*.txt)")
-        if ok1:
-            #self.output_file.setText(file)
-            print(file, ok1, self.layout1.count())
-            # 读取excel 文件语言列表
-
-    def read_excel_file(self):
-        pass
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Little_tool()
+    window = Demo()
     window.show()
     sys.exit(app.exec_())
--- ex_pygui/example4.py
+++ ex_pygui/example4.py
@@ -2,8 +2,10 @@
 #-*- coding: utf-8 -*-
 
 import sys
+
+from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, qApp,\
-    QTextEdit
+    QTextEdit, QPushButton
 from PyQt5.QtGui import QIcon
 
 
@@ -13,8 +15,8 @@ class Example(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        textEdit = QTextEdit()
-        self.setCentralWidget(textEdit)
+        #textEdit = QTextEdit()
+        #self.setCentralWidget(textEdit)
 
         self.statusBar().showMessage('Ready')
         exitAction = QAction(QIcon('breath_time.png'), '&Exit', self)
